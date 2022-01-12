85d40bc265d3e9640c44807038d4726d59aad26a
eduardonk9999 <thiago.novak7@gmail.com>
2022-01-12 15:35:25 -0300

    Add

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Catalog: React.FC = () => {
+  //const [catalog, setCatalog] = useState<>([]);  
+
+  return (
+    <h1>Catalog</h1>
+  )
+}
+
+export default Catalog;
